6feab6cda9e6ed570b8a9f0b1107e51ec2078ac8
Matheus Sena Vasconcelos <sena.matheus14@gmail.com>
2020-05-17 20:22:05 -0300

    remove static and templates declaration from Blueprints

--- project/backend/application1/__init__.py
+++ project/backend/application1/__init__.py
@@ -1,15 +1,10 @@
-import os
-
 from flask import Blueprint
-
-from ...configuration import BASE_DIR
 
 # import your views route here
 
 app_application1 = Blueprint('application1', __name__,
-                             url_prefix='/',
-                             static_folder='static',
-                             static_url_path=os.path.join(BASE_DIR, 'project', 'frontend', 'static'),
-                             template_folder=os.path.join(BASE_DIR, 'project', 'frontend', 'templates'))
+                             url_prefix='/')
 
 # add your views route here
+
+# add your error views here
